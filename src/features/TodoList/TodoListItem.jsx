92395ad226a5e9a427f8608086a52192a4a7e106
hellosamm <142949327+hellosamm@users.noreply.github.com>
2025-08-19 11:34:21 -1000

    Toggle between Todo Display and Edit p2

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -3,24 +3,41 @@ import TextInputWithLabel from "../../shared/TextInputWithLabel";
 
 export default function TodoListItem({ todo, onCompleteTodo }) {
   const [isEditing, setIsEditing] = useState(false);
+  const [workingTitle, setWorkingTitle] = useState(todo.title);
+
+  const handleCancel = () => {
+    setWorkingTitle(todo.title);
+    setIsEditing(false);
+  };
 
   if (!todo) return null;
   if (!todo.title) return null;
 
   return (
     <div>
-      {isEditing ? (
-        <TextInputWithLabel value={todo.title} />
-      ) : (
-        <form onSubmit={(e) => e.preventDefault()}>
-          <input
-            type="checkbox"
-            checked={todo.isCompleted}
-            onChange={() => onCompleteTodo(todo.id)}
-          />
-          <span onClick={() => setIsEditing(true)}>{todo.title}</span>
+      <li>
+        <form>
+          {isEditing ? (
+            <>
+              <TextInputWithLabel value={todo.title} />
+              <button type="button" onClick={() => handleCancel()}>
+                Cancel
+              </button>
+            </>
+          ) : (
+            <>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={todo.isCompleted}
+                  onChange={() => onCompleteTodo(todo.id)}
+                />
+              </label>
+              <span onClick={() => setIsEditing(true)}>{todo.title}</span>
+            </>
+          )}
         </form>
-      )}
+      </li>
     </div>
   );
 }
