d5758ed49c78759ae06553a3f7476cfd7720f329
hellosamm <142949327+hellosamm@users.noreply.github.com>
2025-08-19 11:24:05 -1000

    Toggle between Todo Display and Edit p1

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import TextInputWithLabel from "../../shared/TextInputWithLabel";
 
 export default function TodoListItem({ todo, onCompleteTodo }) {
+  const [isEditing, setIsEditing] = useState(false);
+
   if (!todo) return null;
   if (!todo.title) return null;
 
   return (
     <div>
-      {/* <li> */}
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input
-          type="checkbox"
-          checked={todo.isCompleted}
-          onChange={() => onCompleteTodo(todo.id)}
-        />
-        {todo.title}
-      </form>
-      {/* </li> */}
+      {isEditing ? (
+        <TextInputWithLabel value={todo.title} />
+      ) : (
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input
+            type="checkbox"
+            checked={todo.isCompleted}
+            onChange={() => onCompleteTodo(todo.id)}
+          />
+          <span onClick={() => setIsEditing(true)}>{todo.title}</span>
+        </form>
+      )}
     </div>
   );
 }
